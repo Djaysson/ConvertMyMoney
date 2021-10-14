dca8be597347aa158c8747ad12ff4962918ca998
Djaysson <djayson.dev@gmail.com>
2021-10-14 14:04:13 -0300

    Refactoring files and making project interface improvements

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require("express");
+const app = express();
+const path = require("path");
 
-const convert = require('./lib/convert')
-const apiBCB = require('./lib/api-bcb')
+const { convert, toMoney } = require("./lib/convert");
+const apiBCB = require("./lib/api-bcb");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
-app.use(express.static(path.join(__dirname, 'public')))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', async (req, res) => {
-  const cotacao = await apiBCB.getCotacao()
-  res.render('home', {
-    cotacao
-  })
-})
-app.get('/cotacao', (req, res) => {
-  const { cotacao, quantidade } = req.query
+app.get("/", async (req, res) => {
+  const cotacao = await apiBCB.getCotacao();
+  res.render("home", {
+    cotacao,
+    type: "initial",
+  });
+});
+app.get("/cotacao", (req, res) => {
+  const { cotacao, quantidade } = req.query;
   if (cotacao && quantidade) {
-    const conversao = convert.convert(cotacao, quantidade)
-    res.render('cotacao', {
+    const conversao = convert(cotacao, quantidade);
+    res.render("home", {
+      cotacao: toMoney(cotacao),
+      quantidade: toMoney(quantidade),
+      conversao: toMoney(conversao),
       error: false,
-      cotacao: convert.toMoney(cotacao),
-      quantidade: convert.toMoney(quantidade),
-      conversao: convert.toMoney(conversao)
-    })
+      type: "result",
+    });
   } else {
-    res.render('cotacao', {
-      error: 'Valores inválidos',
-
-    })
+    res.render("cotacao", {
+      error: "Valores inválidos",
+    });
   }
-})
+});
 
-app.listen(port, err => {
+app.listen(port, (err) => {
   if (err) {
-    console.log('Não foi possivel iniciar')
+    console.log("Não foi possivel iniciar");
   } else {
-    console.log('ConvertMyMoney esta online')
+    console.log("ConvertMyMoney esta online");
   }
-})
+});
